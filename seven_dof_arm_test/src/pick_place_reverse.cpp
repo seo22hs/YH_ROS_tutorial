81d069cd04de847da1e5254b90ca96608487c1fb
seo22hs <lsc584727@gmail.com>
2026-03-17 02:07:42 -0700

    실습 5: Pick & Place 변형 코드 추가 - y좌표 변경

--- seven_dof_arm_test/src/pick_place_reverse.cpp
+++ seven_dof_arm_test/src/pick_place_reverse.cpp
@@ -44,14 +44,14 @@ int main(int argc, char **argv) {
 
 	string line;
 
-	// Waiting for scene initialization
+	//Waiting for scene initialization
 	sleep(2);
 
 	//--- objects into the scene
 	moveit::planning_interface::PlanningSceneInterface current_scene;
 	geometry_msgs::Pose pose;
 
-	//--- Add grasping object to the scene
+	//---Add grasping object to the scene
 	shape_msgs::SolidPrimitive primitive;
 	primitive.type = primitive.BOX;
 	primitive.dimensions.resize(3);
@@ -62,8 +62,8 @@ int main(int argc, char **argv) {
 	moveit_msgs::CollisionObject grasping_object;
 	grasping_object.id = "grasping_object";
 	pose.orientation.w = 1.0;
-	pose.position.x = 0.38;
 	pose.position.y = -0.1;
+	pose.position.x = 0.41;
 	pose.position.z = 0.35;
 
 	grasping_object.primitives.push_back(primitive);
@@ -75,7 +75,6 @@ int main(int argc, char **argv) {
 	primitive.dimensions[0] = 0.3;
 	primitive.dimensions[1] = 0.5;
 	primitive.dimensions[2] = 0.32;
-
 	moveit_msgs::CollisionObject grasping_table;
 	grasping_table.id = "grasping_table";
 	pose.position.y = 0.0;
@@ -90,20 +89,22 @@ int main(int argc, char **argv) {
 	collision_objects.push_back(grasping_object);
 	collision_objects.push_back(grasping_table);
 
+	// Once all of the objects (in this case just one) have been added to the
+	// vector, we tell the planning scene to add our new box
 	current_scene.addCollisionObjects(collision_objects);
 
 	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 	const robot_state::JointModelGroup *joint_model_group =
 		group.getCurrentState()->getJointModelGroup("arm");
 
-	//--- approaching
+	//---approaching
 	geometry_msgs::Pose target_pose;
 	target_pose.orientation.x = 0;
 	target_pose.orientation.y = 0;
 	target_pose.orientation.z = 0;
 	target_pose.orientation.w = 1;
-	target_pose.position.x = 0.28;
 	target_pose.position.y = -0.1;
+	target_pose.position.x = 0.28;
 	target_pose.position.z = 0.35;
 	group.setPoseTarget(target_pose);
 	group.move();
@@ -111,16 +112,17 @@ int main(int argc, char **argv) {
 
 	cout << "First motion done!" << endl;
 
-	//--- grasping
-	target_pose.position.x = 0.34;
+	//---grasping
 	target_pose.position.y = -0.1;
+	target_pose.position.x = 0.34;
 	target_pose.position.z = 0.35;
 	group.setPoseTarget(target_pose);
 	group.move();
 
 	cout << "attacching!" << endl;
 
-	//--- attach object to the robot
+	//---attach object to the robot
+	//grasping_object
 	moveit_msgs::AttachedCollisionObject att_coll_object;
 	att_coll_object.object.id = "grasping_object";
 	att_coll_object.link_name = "gripper_finger_link1";
@@ -130,46 +132,43 @@ int main(int argc, char **argv) {
 	sleep(2);
 
 	cout << "second motion!" << endl;
-
-	//--- move up higher after grasping
-	target_pose.position.x = 0.34;
+	//---move far away from the grasping position
 	target_pose.position.y = -0.1;
-	target_pose.position.z = 0.5;
+	target_pose.position.x = 0.34;
+	target_pose.position.z = 0.4;
 	group.setPoseTarget(target_pose);
 	group.move();
 	sleep(2);
 
 	cout << "Picking motion!" << endl;
 
-	//--- diagonal picking move
+	//---picking
 	target_pose.orientation.x = -1;
 	target_pose.orientation.y = 0;
 	target_pose.orientation.z = 0;
 	target_pose.orientation.w = 0;
-	target_pose.position.x = 0.38;
 	target_pose.position.y = 0.1;
-	target_pose.position.z = 0.5;
+	target_pose.position.x = 0.34;
+	target_pose.position.z = 0.4;
 	group.setPoseTarget(target_pose);
 	group.move();
 
 	cout << "Picked!" << endl;
 
-	//--- lower object at new place
-	target_pose.position.x = 0.38;
 	target_pose.position.y = 0.1;
+	target_pose.position.x = 0.34;
 	target_pose.position.z = 0.35;
 	group.setPoseTarget(target_pose);
 	group.move();
 
-	//--- remove object from robot's body
+	//---remove object from robot's body
 	att_coll_object.object.operation = att_coll_object.object.REMOVE;
 	att_coll_object.link_name = "gripper_finger_link1";
 	att_coll_object.object.id = "grasping_object";
 	planning_scene_interface.applyAttachedCollisionObject(att_coll_object);
 
-	//--- final move after release
-	target_pose.position.x = 0.32;
 	target_pose.position.y = 0.1;
+	target_pose.position.x = 0.32;
 	target_pose.position.z = 0.35;
 	group.setPoseTarget(target_pose);
 	group.move();
